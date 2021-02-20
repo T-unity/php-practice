dcde9ffecb11fc6fcf181104a1e0e2feb06ed3e0
T-unity <takadanaritoshi@gmail.com>
2021-02-21 08:08:22 +0900

    commit

---
+++ arr.php
@@ -0,0 +1,24 @@
+<?php
+
+// $arr = [1,2,3];
+// $arr[] = 4;
+
+// // var_export($arr);
+// // echo $arr . PHP_EOL;
+
+// $comics = ['スラムダンク','火の鳥'];
+// $comics[] = '北斗の拳';
+// // var_export($comics);
+
+// echo $comics[2];
+
+$user = [
+  'name' => 'Takada',
+  'sex' => 'male',
+];
+
+$user['age'] = 20;
+
+// var_export($user);
+
+echo $user['name'] . PHP_EOL;
