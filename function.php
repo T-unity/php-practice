8cc556821ed61e5735dcf4239a17e14f2e27bcd6
T-unity <takadanaritoshi@gmail.com>
2021-02-21 12:26:29 +0900

    commit

---
+++ function.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// function add($a,$b)
+// {
+//   $result = $a + $b;
+//   return $result;
+// }
+
+// $i = add(1,2);
+// echo $i . PHP_EOL;
+
+function multiply($a,$b)
+{
+  return $a * $b;
+}
+
+echo multiply(3,5) . PHP_EOL;
